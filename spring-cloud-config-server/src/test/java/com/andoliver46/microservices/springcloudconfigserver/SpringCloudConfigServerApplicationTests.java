53bcb10db7e5371684aa388e1fae32f6b7c0d50f
AndOliver46 <anderson_souza75@live.com>
2024-01-22 16:22:19 -0300

    feat: config server service created, configured port 8888 and service name in properties

---
+++ spring-cloud-config-server/src/test/java/com/andoliver46/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andoliver46.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
