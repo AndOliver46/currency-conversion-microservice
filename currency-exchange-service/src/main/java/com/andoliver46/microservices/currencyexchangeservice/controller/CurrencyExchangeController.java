281089527e88dbb868814c9f5eafe896ce7e28a0
AndOliver46 <anderson_souza75@live.com>
2024-01-24 15:10:56 -0300

    feat: created repository query method to find with from and to columns, injected currencyExchange repo in controller to get data from the database

--- currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,26 +1,29 @@
 package com.andoliver46.microservices.currencyexchangeservice.controller;
 
-import java.math.BigDecimal;
-
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andoliver46.microservices.currencyexchangeservice.domain.CurrencyExchange;
+import com.andoliver46.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 public class CurrencyExchangeController {
 	
 	private final Environment environment;
+	private CurrencyExchangeRepository currencyExchangeRepository;
 	
-	public CurrencyExchangeController(Environment environment) {
+	public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository currencyExchangeRepository) {
 		this.environment = environment;
+		this.currencyExchangeRepository = currencyExchangeRepository;
 	}
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange getExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to) {
-		CurrencyExchange currencyExchange = new CurrencyExchange(1001L, from, to, BigDecimal.valueOf(65));
+		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to).orElseThrow(() -> new EntityNotFoundException("Unable to find data for " + from + " to " + to));
 		currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
 		return currencyExchange;
 	}
--- currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/repository/CurrencyExchangeRepository.java
+++ currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/repository/CurrencyExchangeRepository.java
@@ -1,8 +1,11 @@
 package com.andoliver46.microservices.currencyexchangeservice.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.andoliver46.microservices.currencyexchangeservice.domain.CurrencyExchange;
 
 public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+	Optional<CurrencyExchange> findByFromAndTo(String from, String to);
 }
