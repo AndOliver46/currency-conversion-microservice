b5d0ebf3ed0c60529518929fb5b6b7b1dab5644d
AndOliver46 <anderson_souza75@live.com>
2024-01-30 14:14:10 -0300

    feat: configured a rate limit of how many times this method is called in a certain time period, and after that configured bulkhead, this annotation sets how many concurrent calls can be mande to this instance

--- currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -17,11 +19,14 @@ public class CircuitBreakerController {
 	
 	@GetMapping("/sample-api")
 	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	//@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	//@RateLimiter(name = "default") //Limits the number of calls in a certain time limit configured in properties file
+	@Bulkhead(name = "default")
 	public String sampleApi() {
 		logger.info("Sample API call received");
-		ResponseEntity<String> returnedEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-		return returnedEntity.getBody();
+		//ResponseEntity<String> returnedEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+		//return returnedEntity.getBody();
+		return "Sample API";
 	}
 	
 	public String hardcodedResponse(Exception ex) {
