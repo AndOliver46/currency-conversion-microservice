808a49b07abfa897577e999436c20474ff9ceb9a
AndOliver46 <anderson_souza75@live.com>
2024-01-30 12:09:06 -0300

    feat: added retry annotation in sample-api endpoint, with custom config in properties: retries number, retries interval, interval exponential time. Created a fallback method that receives a exception and manages the fallback response

--- currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,14 +1,29 @@
 package com.andoliver46.microservices.currencyexchangeservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@GetMapping("/sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
 	public String sampleApi() {
-		return "Sample API";
+		logger.info("Sample API call received");
+		ResponseEntity<String> returnedEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+		return returnedEntity.getBody();
+	}
+	
+	public String hardcodedResponse(Exception ex) {
+		return "FallBack Response: " + ex.getLocalizedMessage();
 	}
 	
 }
