62be6e73ec04799e947911ebf697889d57828126
AndOliver46 <anderson_souza75@live.com>
2024-01-30 13:45:09 -0300

    feat: changing from retry annotation to circuitbreaker annotation with default settings, included a commented annotation that defines the percentage of fail to open circuit

--- currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -15,7 +16,8 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/sample-api")
-	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
 	public String sampleApi() {
 		logger.info("Sample API call received");
 		ResponseEntity<String> returnedEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
