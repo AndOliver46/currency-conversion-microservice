17a7355844d317fd7aa1b756ab76d6740e6513a4
AndOliver46 <anderson_souza75@live.com>
2024-01-31 09:52:30 -0300

    feat: added dependencies: observation, otel bridge, zipkin exporter in currency exchange service, added two properties, percentage of requests tath will be traced and the loggin pattern, started zipkin container

--- currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/andoliver46/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.andoliver46.microservices.currencyexchangeservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,8 @@ import jakarta.persistence.EntityNotFoundException;
 @RestController
 public class CurrencyExchangeController {
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	private final Environment environment;
 	private CurrencyExchangeRepository currencyExchangeRepository;
 	
@@ -23,6 +27,7 @@ public class CurrencyExchangeController {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange getExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to) {
+		logger.info("getExchangeValue called with {} to {}", from, to);
 		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to).orElseThrow(() -> new EntityNotFoundException("Unable to find data for " + from + " to " + to));
 		currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
 		return currencyExchange;
